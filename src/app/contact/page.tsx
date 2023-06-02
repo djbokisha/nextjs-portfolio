40f7c5c49771e04b1864265ad6890f96e42ee22b
djbokisha <bojan.bokisha@gmail.com>
2023-06-02 18:41:13 +0200

    contact page done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import styles from './page.module.css'
-
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "../../components/Button/Button";
 
 function Contact() {
   return (
-    <div className={styles.contact}>Contact</div>
-  )
+    <div className={styles.contact}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="" fill={true} className={styles.img} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
